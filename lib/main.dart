fe69acb83190e99a8032b67262d0c35f2762a37b
Andrew <andrew.j.price@outlook.com>
2020-01-17 20:53:13 -0800

    Add Total Amount Input Field

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:moneytextformfield/moneytextformfield.dart';
 
 void main() => runApp(MaterialApp(title: 'Tip Calculator', home: Gratuity()));
 
 class Gratuity extends StatelessWidget {
   // This widget is the root of your application.
+  TextEditingController mycontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    TextStyle _ts = TextStyle(fontSize: 26.0);
     double billAmt = 0.0;
 
-    TextField billAmtField = TextField(
-      keyboardType: TextInputType.number,
-      onChanged: (String value) {
-        try {
-          billAmt = double.parse(value);
-        } catch (exception) {
-          billAmt = 0.0;
-        }
-      },
-      decoration: InputDecoration(labelText: 'Bill Amount (\$)'),
+    MoneyTextFormField moneyTextField =  MoneyTextFormField(
+      settings: MoneyTextFormFieldSettings(
+          controller: mycontroller,
+          moneyFormatSettings: MoneyFormatSettings(
+              currencySymbol: 'IDR',
+              displayFormat: MoneyDisplayFormat.symbolOnLeft),
+          appearanceSettings: AppearanceSettings(
+              padding: EdgeInsets.all(15.0),
+              labelText: 'Total',
+              hintText: 'Amount',
+              labelStyle: _ts,
+              inputStyle: _ts.copyWith(color: Colors.black),
+              formattedStyle:
+                  _ts.copyWith(color: Colors.green))),
     );
 
     Container container = Container(
       child: Column(
-        children: [billAmtField]
+        children: [moneyTextField]
       )
     );
 
-    AppBar appBar = AppBar(title: Text('Tip Calculator'));
+    AppBar appBar = AppBar(title: Text('Gratuity Calculator'));
     Scaffold scaffold = Scaffold(appBar: appBar, body: container);
     return scaffold;
   }
