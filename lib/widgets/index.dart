6cb0b0575b0c75cd238bc1ced7a644d9274b29f5
Andrew <andrew.j.price@outlook.com>
2020-05-17 11:29:17 -0700

    Remove money_text_field pub and previous unused widgets

--- lib/widgets/index.dart
+++ lib/widgets/index.dart
@@ -1,6 +1,4 @@
 export 'tip_button.dart';
-export 'currency_text_field.dart';
-export 'tip_total_display.dart';
 export 'bill_total_field.dart';
 export 'wave_container.dart';
 export 'tip_button.dart';
