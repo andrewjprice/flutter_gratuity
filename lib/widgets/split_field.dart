c9dfc7a4cb35cfbef28cec5d0386b164360c473b
Andrew <andrew.j.price@outlook.com>
2020-05-23 22:54:17 -0700

    Replace Padding widget with SizeBox

--- lib/widgets/split_field.dart
+++ lib/widgets/split_field.dart
@@ -3,49 +3,48 @@ import 'package:gratuity/index.dart';
 class SplitField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppState>(
-      builder: (context, provider, child) {
-        return Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * .25),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Split',
-                    style: GoogleFonts.tajawal(
-                      fontSize: 28.0,
-                      color: Colors.black26,
-                    )),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    IconButton(
-                        icon: Icon(Icons.remove),
-                        iconSize: 28.0,
-                        color: Theme.of(context).accentColor,
-                        onPressed: () { provider.decreaseSplit(1); }),
-                    SizedBox(width: 10.0),
-                    Padding(
-                      padding: EdgeInsets.only(top: 7.0),
-                      child: Text(provider.split.toString(),
-                          style: GoogleFonts.tajawal(
-                              fontSize: 28.0,
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).accentColor)),
-                    ),
-                    SizedBox(width: 10.0),
-                    IconButton(
-                        icon: Icon(Icons.add),
-                        iconSize: 28.0,
-                        color: Theme.of(context).accentColor,
-                        onPressed: () { provider.increaseSplit(1); }),
-                  ],
-                )
-              ]),
-        );
-      }
-    );
+    return Consumer<AppState>(builder: (context, provider, child) {
+      return Container(
+        margin: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * .25),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Split',
+                  style: GoogleFonts.tajawal(
+                    fontSize: 28.0,
+                    color: Colors.black26,
+                  )),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                      icon: Icon(Icons.remove),
+                      iconSize: 28.0,
+                      color: Theme.of(context).accentColor,
+                      onPressed: () {
+                        provider.decreaseSplit(1);
+                      }),
+                  SizedBox(height: 7.0, width: 10.0),
+                  Text(provider.split.toString(),
+                      style: GoogleFonts.tajawal(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).accentColor)),
+                  SizedBox(width: 10.0),
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      iconSize: 28.0,
+                      color: Theme.of(context).accentColor,
+                      onPressed: () {
+                        provider.increaseSplit(1);
+                      }),
+                ],
+              )
+            ]),
+      );
+    });
   }
 }
